ad4ed9476525c755c4c7c8f9ef19fdf605f07b3f
Adibhange <bhangeaditya13@gmail.com>
2024-12-08 09:13:20 +0530

    Created Error Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,8 +2,15 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
 	return (
-		<div>
-			<h1>Error Page</h1>
+		<div className='flex flex-col items-center justify-center h-screen gap-2'>
+			<h1 className='text-4xl text-copy-lighter tracking-tight'>
+				Page not found
+			</h1>
+			<Link to='/'>
+				<button className='px-4 py-2 bg-primary-light text-primary-content font-semibold rounded-md hover:bg-primary'>
+					Back to Home
+				</button>
+			</Link>
 		</div>
 	);
 };
