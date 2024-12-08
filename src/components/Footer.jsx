db124d4aea2ac13b2fae505a639a9494fdaeecec
Adibhange <bhangeaditya13@gmail.com>
2024-12-08 13:37:17 +0530

    Made some change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className='bg-foreground text-copy-light py-4 px-4 text-center'>
+		<footer className='bg-foreground text-copy-light py-4 px-4 text-center mt-6'>
 			<h1>&copy;2024 Aditya Bhange | All rights reserved.</h1>
 		</footer>
 	);
