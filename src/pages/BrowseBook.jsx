419d96d65586c7d47398b965a3cbf5d341a30f42
Adibhange <bhangeaditya13@gmail.com>
2024-12-08 12:40:47 +0530

    Create CategoryBooks page and also add filter in browsebook page

--- src/pages/BrowseBook.jsx
+++ src/pages/BrowseBook.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import bookCategories from "../data/bookCategories";
 
 const BrowseBook = () => {
-	return <div>BrowseBook</div>;
+	return (
+		<section className='container flex flex-col gap-3 justify-center py-6 px-4 w-[95%] mx-auto bg-foreground rounded-lg shadow-md'>
+			<p className='text-xl text-center text-primary '>
+				Filter Books by Category
+			</p>
+			<div className='flex w-full gap-2'>
+				{bookCategories.map((category) => {
+					return (
+						<div
+							key={category.id}
+							className='rounded-md border-2 border-border bg-foreground px-4 py-2 shadow-md hover:shadow-lg transition-shadow duration-300'>
+							<Link
+								to={`/books/${category.name}`}
+								className='text-xl  '>
+								{category.name}
+							</Link>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default BrowseBook;
--- src/pages/CategoryBooks.jsx
+++ src/pages/CategoryBooks.jsx
@@ -1,8 +1,32 @@
 import { useParams } from "react-router-dom";
+import books from "../data/bookData";
+import BookItem from "../components/BookItem";
 
 const CategoryBooks = () => {
 	const { category } = useParams();
-	return <div>{category}</div>;
+	return (
+		<section className='container flex flex-col gap-3 justify-center py-6 px-4 mx-auto'>
+			<h1 className='text-3xl font-bold text-center'>{category}</h1>
+			<div className='grid grid-cols-4 gap-4'>
+				{books
+					.filter((book) => book.category === category)
+					.map((book) => {
+						return (
+							<BookItem
+								key={book.id}
+								bookId={book.id}
+								bookTitle={book.title}
+								bookAuthor={book.author}
+								bookCoverImage={book.coverImage}
+								bookDescription={book.description}
+								bookCategory={book.category}
+								bookRating={book.rating}
+							/>
+						);
+					})}
+			</div>
+		</section>
+	);
 };
 
 export default CategoryBooks;
